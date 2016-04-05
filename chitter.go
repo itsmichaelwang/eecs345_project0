4108ed0fd2a2ab97dafff5d35c18c31d8e7d3b7d
itsmichaelwang <michaelwang2016@u.northwestern.edu>
2016-04-04 22:40:20 -0500

    Implement whoami:, all:, and pm commands

--- chitter.go
+++ chitter.go
@@ -6,6 +6,7 @@ import (
     "bufio"
     "strconv"
     "fmt"
+    "strings"
 )
 
 type userDetails struct {
@@ -13,18 +14,26 @@ type userDetails struct {
   conn net.Conn
 }
 
+type message struct {
+  sender string
+  recipient string
+  body string
+}
+
 var userList map[string] net.Conn         // keep track of connected users
 
 var idAssignmentChan = make(chan string)  // used to give IDs to all users
 var addUserChan = make(chan userDetails)  // used to add user to userList
+var broadcastMsgChan = make(chan message) // used to broadcast public messages
+var privateMsgChan = make(chan message)   // used to send private messages
 
-func HandleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn) {
     b := bufio.NewReader(conn)
     clientID := <-idAssignmentChan
 
     // create a struct to represent a new user and pass it along to userList
     newUser := userDetails{id: clientID, conn: conn}
-    addUserChan <- newUser
+    addUserChan<-newUser
 
     for {
         line, err := b.ReadBytes('\n')
@@ -33,9 +42,38 @@ func HandleConnection(conn net.Conn) {
             break
         }
 
-        // every time a message is sent, broadcast it to everyone in userList
-        for _, v := range userList {
-          v.Write([]byte(clientID + ": " +string(line)))
+        lineStr := string(line)
+
+        // if message has no colon, broadcast it...
+        if (!strings.Contains(lineStr, ":")) {
+          // ... to everyone in userList
+          broadcastMsg := message{sender: clientID, body: string(line)}
+          broadcastMsgChan<-broadcastMsg
+
+        } else {
+          // otherwise, parse commands by first colon
+          lineSplit := strings.SplitN(lineStr, ":", 2)
+
+          // strip leading and trailing whitespace
+          command := strings.Trim(lineSplit[0], " ")
+
+          // all:
+          if (command == "all") {
+            body := strings.Trim(lineSplit[1], " ")
+            broadcastMsg := message{sender: clientID, body: body}
+            broadcastMsgChan<-broadcastMsg
+
+          // whoami:
+          } else if (command == "whoami") {
+            conn.Write([]byte("chitter: " + clientID + "\n"))
+
+          // private message (e.g. 0:)
+          } else if _, err := strconv.Atoi(command); err == nil {
+            body := strings.Trim(lineSplit[1], " ")
+            privateMsg := message{sender: clientID, recipient: command, body: body}
+            privateMsgChan<-privateMsg
+
+          }
         }
     }
 }
@@ -47,12 +85,19 @@ func userListManager() {
     select {
     case newUser := <-addUserChan:
       userList[newUser.id] = newUser.conn
-      fmt.Println(userList)
+    case broadcastMsg := <-broadcastMsgChan:
+      for _, conn := range userList {
+        conn.Write([]byte(broadcastMsg.sender + ": " + broadcastMsg.body))
+      }
+    case privateMsg := <-privateMsgChan:
+      if conn, ok := userList[privateMsg.recipient]; ok {
+        conn.Write([]byte(privateMsg.sender + ": " + privateMsg.body))
+      }
     }
   }
 }
 
-func IdManager() {
+func idManager() {
     var i uint64
     for i = 0;  ; i++ {
         idAssignmentChan <- strconv.FormatUint(i, 10)
@@ -72,12 +117,12 @@ func main() {
         os.Exit(1)
     }
 
-    go IdManager()
+    go idManager()
     go userListManager()
 
     fmt.Println("Listening on port", os.Args[1])
     for {
         conn, _ := server.Accept()
-        go HandleConnection(conn)
+        go handleConnection(conn)
     }
 }
